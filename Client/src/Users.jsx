a5e2b7cb7e70f8da399eea3f6de570edaf4b53ac
Kushan Dhananjaya <100125038+IT21157882@users.noreply.github.com>
2023-10-10 23:32:16 +0530

    Create_Delete Function

--- Client/src/Users.jsx
+++ Client/src/Users.jsx
@@ -10,6 +10,14 @@ function Users() {
         .then(result => setUsers(result.data))
         .catch(err => console.log(err))
     }, [])
+
+    const handleDelete = (id) => {
+        axios.delete('http://localhost:3001/deleteUser/'+id)
+        .then(res => {console.log(res)
+            window.location.reload()})   
+        .catch(err => console.log(err))
+    }
+
     return(
   <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
     <div className='w-50 bg-white rounded p-3'>
@@ -32,7 +40,8 @@ function Users() {
                         <td>{user.age}</td>
                         <td>
                             <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link>
-                            <button className='btn btn-danger'>Delete</button>
+                            <button className='btn btn-danger'
+                             onClick={(e) => handleDelete(user._id)}>Delete</button>
                         </td>
                     </tr>
                 )})}
